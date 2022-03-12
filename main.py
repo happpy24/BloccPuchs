3f31969ccfd34e1152cca342e75f9da47bc768aa
Vincent Speijer <69733685+happpy24@users.noreply.github.com>
2022-03-12 22:50:23 +0000

    levelLogic ofzo idk wat ik aan het doen ben tbh

--- main.py
+++ main.py
@@ -58,12 +58,27 @@ levels = {
 
 currentLevel = ("level1")
 
+def levelLogic():
+	if player.level == "level1":
+		llList = list(levels[currentLevel][map][levelLine2])
+		llList[6] = "🐁"
+		llList[7] = ""
+		levels[currentLevel][map][levelLine2] = "".join(llList)
+	if player.level == "level2":
+		pass
+
 def createLevel():
+
+	
+	
 	print(levelLineWall)
 	for x in levels[currentLevel][map]:
 		print(levels[currentLevel][map][x])
 	print(levelLineWall)
 
+def movement():
+	pass
+
+
+levelLogic()
 createLevel()
-
-
